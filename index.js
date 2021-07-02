6d9ecf2f58ddcd40a0cc42e448d7ab3100aca0c4
Rajkumar Rupapara <rajrupapara778@gmail.com>
2021-07-02 14:34:30 +0530

    olympic simple API

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const options = {
         servers: [
             {
 
-                url: "http://localhost:3000"
+                url: "https://olympic7api.herokuapp.com/api-docs/"
             }
         ],
     },
